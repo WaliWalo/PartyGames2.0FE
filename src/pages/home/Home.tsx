bbb94fe127bb0c63ce581d0d16a58683bdacc680
Hung Jin Chong <hungjinchong@Outlook.com>
2021-08-11 11:06:11 +0100

    Home page and Loader
    Changed background and minor responsive issues for home page
    Implemented animation for loading into home page

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import {
   Button,
   FormControl,
@@ -12,78 +12,115 @@ import {
 import './home.css';
 import ExitToAppIcon from '@material-ui/icons/ExitToApp';
 import AddBoxIcon from '@material-ui/icons/AddBox';
+import gsap from 'gsap';
+import HomeLoader from './../loader/HomeLoader';
 
 function Home() {
   const matches = useMediaQuery('(max-width: 426px)');
-  const [game, setGame] = React.useState('Truth or Dare');
+  const [game, setGame] = useState('Truth or Dare');
+
   const handleChange = (event: React.ChangeEvent<{ value: unknown }>) => {
     setGame(event.target.value as string);
   };
+
+  useEffect(() => {
+    gsap.to('#party', {
+      x: '130%',
+      duration: 1.5,
+      ease: 'bounce.out',
+      onComplete: () => {
+        gsap.to('#games', {
+          x: '150%',
+          duration: 1.5,
+          ease: 'bounce.out',
+          onComplete: () => {
+            gsap.to('#homeLoader', {
+              opacity: 0,
+              display: 'none',
+              duration: 1,
+            });
+          },
+        });
+      },
+    });
+  }, [matches]);
+
   return (
-    <div className="App">
-      <div className="header">
-        <h1>PARTY GAMES</h1>
-      </div>
-      <div className="home-form">
-        <div className="join-room">
-          <form>
-            <div className="create-fields">
-              <TextField
-                id="filled-basic-username"
-                label="Username"
-                variant="filled"
-                size="medium"
-                required
-              />
-              <TextField
-                id="filled-basic-room-name"
-                label="Room Id"
-                variant="filled"
-                size="medium"
-                required
-              />
+    <>
+      <HomeLoader />
+      <video playsInline autoPlay muted loop id="bgvid">
+        <source
+          src="https://res.cloudinary.com/waliwalo/video/upload/v1617028507/solocap/Abstract_animation_pipelines_back_ground_kqhszo.mp4"
+          type="video/mp4"
+        />
+      </video>
+      <div id="homeBg">
+        <div className="App">
+          <div className="header">
+            <h1>PARTY GAMES</h1>
+          </div>
+          <div className="home-form">
+            <div className="join-room">
+              <form>
+                <div className="create-fields">
+                  <TextField
+                    id="filled-basic-username"
+                    label="Username"
+                    variant="filled"
+                    size="medium"
+                    required
+                  />
+                  <TextField
+                    id="filled-basic-room-name"
+                    label="Room Id"
+                    variant="filled"
+                    size="medium"
+                    required
+                  />
+                </div>
+                <Button variant="contained" color="primary">
+                  {matches ? <ExitToAppIcon fontSize="large" /> : 'Join Room'}
+                </Button>
+              </form>
             </div>
-            <Button variant="outlined" color="primary">
-              {matches ? <ExitToAppIcon fontSize="large" /> : 'Join Room'}
-            </Button>
-          </form>
-        </div>
-        <hr />
-        <div className="create-room">
-          <form>
-            <div className="create-fields">
-              <TextField
-                id="filled-basic-username"
-                label="Username"
-                variant="filled"
-                size="medium"
-                required
-              />
-              <FormControl required>
-                <InputLabel id="select-helper-label">Game type</InputLabel>
-                <Select
-                  labelId="select-required-label"
-                  id="select-required"
-                  value={game}
-                  onChange={handleChange}
-                  autoWidth
-                >
-                  <MenuItem value={'Truth or Dare'}>Truth or Dare</MenuItem>
-                  <MenuItem value={'Would You Rather'}>
-                    Would You Rather
-                  </MenuItem>
-                  <MenuItem value={'Bingo'}>Bingo</MenuItem>
-                  <MenuItem value={'Charades'}>Charades</MenuItem>
-                </Select>
-              </FormControl>
+            <hr />
+            <div className="create-room">
+              <form>
+                <div className="create-fields">
+                  <TextField
+                    id="filled-basic-username"
+                    label="Username"
+                    variant="filled"
+                    size="medium"
+                    required
+                  />
+                  <FormControl required>
+                    <InputLabel id="select-helper-label">Game type</InputLabel>
+                    <Select
+                      labelId="select-required-label"
+                      id="select-required"
+                      value={game}
+                      onChange={handleChange}
+                      autoWidth
+                    >
+                      <MenuItem value={'Truth or Dare'}>Truth or Dare</MenuItem>
+                      <MenuItem value={'Would You Rather'}>
+                        Would You Rather
+                      </MenuItem>
+                      <MenuItem value={'Bingo'}>Bingo</MenuItem>
+                      <MenuItem value={'Charades'}>Charades</MenuItem>
+                    </Select>
+                  </FormControl>
+                </div>
+                <Button variant="contained" color="primary">
+                  {matches ? <AddBoxIcon fontSize="large" /> : 'Create Room'}
+                </Button>
+              </form>
             </div>
-            <Button variant="outlined" color="primary">
-              {matches ? <AddBoxIcon fontSize="large" /> : 'Create Room'}
-            </Button>
-          </form>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
 
---
+++ src/pages/loader/HomeLoader.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './homeLoader.css';
+
+function HomeLoader() {
+  return (
+    <div id="homeLoader">
+      <div className="loaderTitle" id="party"></div>
+      <div className="loaderTitle" id="games"></div>
+    </div>
+  );
+}
+
+export default HomeLoader;
