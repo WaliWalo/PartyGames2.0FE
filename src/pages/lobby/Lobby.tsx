2e4693152217fa90bf86f5c4e556dbba5d16138e
Hung Jin Chong <hungjinchong@Outlook.com>
2021-09-15 12:34:37 +0100

    Lobby concept
    Implemented concept with gsap
    Idea is to implement avatar/characters that are able to move in a 2d environment

--- src/pages/lobby/Lobby.tsx
+++ src/pages/lobby/Lobby.tsx
@@ -1,7 +1,54 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
+import { makeStyles } from '@material-ui/core';
+import gsap from 'gsap/all';
 
 function Lobby() {
-  return <div>TEST</div>;
+  const useStyles = makeStyles({
+    container: { backgroundColor: 'blue', height: '100vh' },
+    containerA: {
+      backgroundColor: 'black',
+      height: '10em',
+      width: '10em',
+      // position: 'absolute',
+      // bottom: '0',
+      // zIndex: 5,
+    },
+    inputTest: {
+      opacity: 0,
+      position: 'absolute',
+      bottom: '0',
+      zIndex: 0,
+    },
+  });
+
+  const classes = useStyles();
+
+  const clamper = gsap.utils.clamp(0, 500);
+  let x = 0;
+  const containerOnKeyPress = (e: React.KeyboardEvent<HTMLDivElement>) => {
+    if (e.code === 'ArrowRight') {
+      x += 100;
+      gsap.to(`.${classes.containerA}`, { x: clamper(x) });
+    } else if (e.code === 'ArrowLeft') {
+      x -= 100;
+      gsap.to(`.${classes.containerA}`, { x: clamper(x) });
+    }
+  };
+
+  return (
+    <div className={classes.container}>
+      <div className={classes.containerA}>
+        <input
+          autoFocus
+          className={classes.inputTest}
+          onKeyDown={(e: React.KeyboardEvent<HTMLDivElement>) =>
+            containerOnKeyPress(e)
+          }
+          onBlur={(e) => e.currentTarget.focus()}
+        ></input>
+      </div>
+    </div>
+  );
 }
 
 export default Lobby;
